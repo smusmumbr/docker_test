a9d93bc2540ffdddb5fe46b39fc3c2778e3a88fb
Kostiantyn Afanasiev <kafanasiev@bcdtriptech.com>
2024-05-09 11:20:44 +0300

    refactor tokenizer import and constants

--- sentencer/sentencer/cli/main.py
+++ sentencer/sentencer/cli/main.py
@@ -1,6 +1,10 @@
 import io
 
+from importlib import import_module
+
 import click
+
+from ..constants import SUPPORTED_LANGUAGES, TOKENIZERS
 
 
 @click.group()
@@ -9,23 +13,23 @@ def cli():
 
 
 @cli.command()
-@click.option('-t', '--stype', type=click.Choice(('naive',)), default='naive')
-@click.option('-l', '--language', type=click.Choice(('en', 'de')), default='en')
+@click.option('-t', '--stype', type=click.Choice(TOKENIZERS), default='naive')
+@click.option('-l', '--language', type=click.Choice(SUPPORTED_LANGUAGES), default='en')
 @click.option('-f','--filename',type=click.File(), required=True)
 def tokenize(language: str, filename: io.IOBase, stype: str) -> None:
     _print_tokenized(filename.read(), stype, language)
 
 
 @cli.command()
-@click.option('-t', '--stype', type=click.Choice(('naive',)), default='naive')
+@click.option('-t', '--stype', type=click.Choice(TOKENIZERS), default='naive')
 @click.argument('text')
 def test(text: str, stype: str) -> None:
     _print_tokenized(text, stype, language='en')
     
 
 def _print_tokenized(text: str, stype: str, language: str) -> None:
-    if stype == 'naive':
-        from ..tokenizers.naive import tokenize as impl_tokenize
-    else:
+    try:
+        module = import_module(f'sentencer.tokenizers.{stype}')
+    except ImportError:
         raise ValueError(f'Unknown sentencer type: {stype}')
-    print(impl_tokenize(text, language))
+    print(module.tokenize(text, language))
